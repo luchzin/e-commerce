b6b92da69dd76b52c0c24d43eea71e2599e44436
luchzin <luch34976@gmail.com>
2025-07-17 22:24:47 +0700

    first commit

---
+++ src/components/app-header.tsx
@@ -0,0 +1,34 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "./ui/separator";
+import { SidebarTrigger } from "./ui/sidebar";
+
+export default function AppHeader() {
+  return (
+    <div className="flex flex-row gap-1   items-center h-fit">
+      <SidebarTrigger className="  " />
+      <Separator orientation="vertical" />
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
