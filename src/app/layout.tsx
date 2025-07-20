d326cc7ba98c22b4fb57d780e88a04f9e9cb4136
Yansokchan <prochnjr2299@gmail.com>
2025-07-20 09:30:20 +0700

    Test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ThemeToggle } from "../components/theme-toggle";
-import { SidebarProvider, } from "../components/ui/sidebar";
+import { SidebarProvider } from "../components/ui/sidebar";
 import AppSidebar from "../components/app-sidebar";
 import AppHeader from "../components/app-header";
 
@@ -39,11 +39,10 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <SidebarProvider>
-            <AppSidebar />
+            {/* <AppSidebar /> */}
             <main className="p-2 flex  flex-col ">
               <AppHeader />
               <div className="  py-8 ">
- 
                 <div className="grid items-center">{children}</div>
               </div>
             </main>
